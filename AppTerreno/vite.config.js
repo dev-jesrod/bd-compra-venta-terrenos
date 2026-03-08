526958a9f0a4d9b5fc663621d66216112a2a3114
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-03-07 23:45:24 -0700

    Se limpio un poco el html y se configuro con vite para que use talwin

--- AppTerreno/vite.config.js
+++ AppTerreno/vite.config.js
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/login.css',
+                'resources/js/tailwind-config.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
