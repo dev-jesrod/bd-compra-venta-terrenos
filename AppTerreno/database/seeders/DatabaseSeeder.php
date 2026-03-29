8725479fc1aa3e7d56bb19d98d4d50407077d90e
Soy Daniel <danielleonne.uaprj19@gmail.com>
2026-03-29 10:13:17 -0700

    Se agrego el modelo Usuarios al Seeder

--- AppTerreno/database/seeders/DatabaseSeeder.php
+++ AppTerreno/database/seeders/DatabaseSeeder.php
@@ -1,21 +1,18 @@
-<?php
+<?php 
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::factory(10)->create();
-
+        Usuario::factory(20)->create();
     }
 }
+
