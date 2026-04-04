528dc08fb5ee890dbacf90395992ffe894cf8433
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-04-04 12:24:03 -0700

    Se agrego un mensaje en forma de documentacions

--- AppTerreno/database/migrations/2026_03_21_204406_create_dato_vendedors_table.php
+++ AppTerreno/database/migrations/2026_03_21_204406_create_dato_vendedors_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('dato_vendedores', function (Blueprint $table) {
             $table->id('idVendedor');
 
             $table->foreignId('idUsuario')
-                  ->constrained('usuarios')
-                  ->references('idUsuario');
+                ->constrained('usuarios')
+                ->references('idUsuario');
 
             $table->string('rfc', 13)->unique();
             $table->decimal('utilidad', 10, 2)->unsigned();
+            $table->string('ine', 5)->unique();
 
             $table->timestamps();
         });
