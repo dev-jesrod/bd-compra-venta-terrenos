f060184ac0c757b234fa3da26d03ac0b0b36c943
dev-jesrod <rodolfolizr19@gmail.com>
2026-04-06 23:53:20 -0700

    Añadiendo los campos basicos de formulario al archivo.
    Añadimos el formulario, que funcionara para realizar pruebas.
    Solo añadimos dos campos:
    Correo electronico y contraseña

---
+++ AppTerreno/resources/views/login.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion -- MazTerrenos</title>
+</head>
+
+<body>
+    
+    <h2>Inicio de Sesion</h2>
+<!-- Formulario -->
+    <form action="/" method="get">
+
+        <ul>
+            <li>
+                <label for="correo">Correo electronico:</label>
+                <input type="email" id="correo" name="correo_electronico" required >
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <label for="contrasena">Contraseña:</label>
+                <input type="password" id="contrasena" name="contrasena" required >
+            </li>
+        </ul>
+
+        <li>
+            <button >Enviar</button>
+        </li>
+
+    </form>
+<!-- fin Formulario -->
+
+</body>
+
+</html>
