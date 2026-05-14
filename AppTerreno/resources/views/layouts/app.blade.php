d19567bce3de70e253e3f604879e07401793c5f4
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-05-13 17:47:21 -0700

    REFACTOR: Resuelve conflictos de merge en componentes del vendedor
    Antecedentes:
    Los archivos navbar.blade.php, sidebar.blade.php, layouts/app.blade.php
    y layouts/vendedor.blade.php contenían múltiples bloques de conflictos
    de git merge (<<<<<<< HEAD, =======, >>>>>>>) sin resolver.
    
    Solución:
    Se resolvieron los conflictos manteniendo la versión más reciente (e61e1da5).
    Además, se eliminó el Tailwind config inline de vendedor.blade.php
    (68 líneas) que estaba duplicado en tailwind-config.js.

--- AppTerreno/resources/views/layouts/app.blade.php
+++ AppTerreno/resources/views/layouts/app.blade.php
@@ -14,17 +14,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
         rel="stylesheet" />
 
-    <!-- Vite Directives -->
     @vite([
-<<<<<<< HEAD
-    'resources/css/app.css',
-    'resources/js/app.js'
-=======
         'resources/js/tailwind-config.js',
         'resources/css/app.css',
         isset($css_file) ? "resources/css/{$css_file}.css" : ''
-
->>>>>>> e61e1da5efe97daf3de90e6e8798ef4922300783
     ])
 </head>
 
